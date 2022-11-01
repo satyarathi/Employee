8b93c1025195c7a38a8f91d976f0b146ce2be3b5
satyarathi <jitendrasatyarathi@gmail.com>
2022-11-01 14:59:28 +0530

    second commit

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,8 +1,15 @@
 package com.practice.problem;
-
+import java.util.Random; 
 public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome");
+		Random attendance = new Random(); 
+		int i = attendance.nextInt(2);
+		if(i == 0) {
+			System.out.println("Employee is Abscent");
+		} else {
+			System.out.println("Employee is present");
+		}
 	}
 }
